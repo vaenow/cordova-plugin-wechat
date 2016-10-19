e9b66ad74a5a3d6498caf1417e5df8f4acdf93fe
vaenow <luowenbiz@gmail.com>
2016-10-19 17:54:25 +0800

    update wechat payments

--- www/wechat.js
+++ www/wechat.js
@@ -1,4 +1,7 @@
 var exec = require('cordova/exec');
+var ajax = require('./ajax.build');
+
+var defaultWxResponseURL = "http://192.168.0.222:10086";
 
 module.exports = {
     Scene: {
@@ -93,7 +96,55 @@ module.exports = {
      * });
      * </code>
      */
-    sendPaymentRequest: function (params, onSuccess, onError) {
-        exec(onSuccess, onError, "Wechat", "sendPaymentRequest", [params]);
+    sendPaymentRequest: function(params, onSuccess, onError) {
+        //"http://192.168.0.222:10086/?orderNo=A201610191508&fee=234&item=test&ip=8.8.8.9",
+        $.ajax({
+                method: "get",
+                url: params.resUrl || defaultWxResponseURL,
+                data: {
+                    orderNo: params.orderNo || formatDate((new Date()),"yyyyMMddhhmmss"),
+                    item: params.item || "TestOrder",
+                    fee: params.fee || 234,
+                    ip: params.ip || "8.8.8.8"
+                }
+            })
+            //ajax.getJSON(params.resUrl || defaultWxResponseURL)
+            .success(function(data) {})
+            .fail(function(data) {})
+            .always(function(data) {
+                exec(onSuccess, onError, "Wechat", "sendPaymentRequest", [data]);
+            });
     }
 };
+
+//格式化日期,
+function formatDate(date, format) {
+    var paddNum = function(num) {
+            num += "";
+            return num.replace(/^(\d)$/, "0$1");
+        }
+        //指定格式字符
+    var cfg = {
+        yyyy: date.getFullYear() //年 : 4位
+            ,
+        yy: date.getFullYear().toString().substring(2) //年 : 2位
+            ,
+        M: date.getMonth() + 1 //月 : 如果1位的时候不补0
+            ,
+        MM: paddNum(date.getMonth() + 1) //月 : 如果1位的时候补0
+            ,
+        d: date.getDate() //日 : 如果1位的时候不补0
+            ,
+        dd: paddNum(date.getDate()) //日 : 如果1位的时候补0
+            ,
+        hh: date.getHours() //时
+            ,
+        mm: date.getMinutes() //分
+            ,
+        ss: date.getSeconds() //秒
+    }
+    format || (format = "yyyy-MM-dd hh:mm:ss");
+    return format.replace(/([a-z])(\1)*/ig, function(m) {
+        return cfg[m];
+    });
+}
